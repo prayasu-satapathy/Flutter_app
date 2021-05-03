edc79841b301d81320e5cb3933fddcd3e33c5795
Prayasu Sidha Satapathy <satapathyprayasu@gmail.com>
2021-05-03 12:01:22 +0530

    Add files via upload

---
+++ flutter_app1 main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp
+    (
+    new Center
+      (
+      child:new Text
+        (
+            "HELLO WORLD",
+            textDirection :TextDirection.ltr
+         )
+      )
+     );
+}
